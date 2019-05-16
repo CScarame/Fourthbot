5e5d7c0b9d3e29ac8a1a21f314f45b459975c807
CScarame <CScarame@nd.edu>
2019-05-16 15:27:30 -0700

    Update new_main.py
    main file now automatically loads all cogs when starting
    can now send cmdline arg "-d" in order to run track debug information with logger.

--- new_main.py
+++ new_main.py
@@ -1,8 +1,9 @@
+
 import discord
 from   discord.ext import commands
 
 import logging
-import os
+import os, sys, getopt
 
 bot = commands.Bot(command_prefix='!')
 
@@ -11,7 +12,11 @@ async def on_ready():
     logging.debug("Discord connection made.")
     logging.info("Logging in as:{0} : {1}".format(bot.user.name,bot.user.id))
     print("Logged in as\n{0}\n{1}".format(bot.user.name,bot.user.id))
-    bot.load_extension("Cogs.XP")
+    for file in os.listdir("Cogs"):
+        logging.debug("Loading {}".format(file))
+        file_name = file[:-3]
+        cog_name = ".".join("Cogs",file_name)
+        bot.load_extension(cog_name)
 
 @bot.event
 async def on_command(ctx):
@@ -20,8 +25,15 @@ async def on_command(ctx):
 
 if __name__ == "__main__":
     ## TODO: Set logging level to DEBUG if command line args include --debug or -d
-
-    logging.basicConfig(filename="runtime.log",level=logging.INFO)
+    log_level = logging.INFO
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "d")
+    except getopt.GetoptError:
+        print('error in args')
+    for opt, arg in opts:
+        if opt == '-d':
+            log_level = logging.DEBUG
+    logging.basicConfig(filename="runtime.log",level=log_level)
     logging.debug("Starting program")
     bot.run('NDM4Mjg4NjYxMzk4NjgzNjUw.DcCb5A.gyiEoXkZyEnnByhjOXshRriRHXY')
 
